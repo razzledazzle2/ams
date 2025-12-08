15208e362ff06f4878d29d8d59a0f2ce98800427
root <root@DESKTOP-NTAHMI7.localdomain>
2025-12-08 13:57:46 +1100

    Add dapper context

---
+++ ams-api/Database/DapperContext.cs
@@ -0,0 +1,19 @@
+﻿namespace ams_api;
+
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System.Data;
+
+public class DapperContext
+{
+    private readonly IDbConnection _dbConnection;
+
+    public DapperContext(IConfiguration configuration)
+    {
+        string? connectionString = configuration.GetConnectionString("PostgressConnection");
+        _dbConnection = new NpgsqlConnection(connectionString);
+    }
+
+    public IDbConnection DbConnection => _dbConnection;
+
+}
