058156823b34003c1ee47ab59bf0f958d81c80f9
Raymond <raymondxuan3@gmail.com>
2025-12-17 16:27:47 +1100

    Remove alert when expiry

--- frontend/frontend/src/lib/apiFetch.ts
+++ frontend/frontend/src/lib/apiFetch.ts
@@ -26,7 +26,6 @@ export async function apiFetch(input: RequestInfo, init?: RequestInit) {
     // refresh expires or invalid --> logout
     if (!refresh.ok) {
       logout();
-      alert("Session expired. Please log in again.");
       throw new Error("Session expired");
     }
     // save new access token
