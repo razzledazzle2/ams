6d0f763f7b4f083ba1bdf7791db81c4b8ee11fe2
Raymond <raymondxuan3@gmail.com>
2025-12-23 09:58:00 +1100

    fix: Fixed icon should be eyeoff to show password instead of eye

--- frontend/frontend/src/components/Login.tsx
+++ frontend/frontend/src/components/Login.tsx
@@ -76,7 +76,7 @@ export const Login = () => {
                 onClick={() => setShowPassword((prev) => !prev)}
                 className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
               >
-                {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+                {showPassword ? <Eye size={18} /> : <EyeOff size={18} />}
               </button>
             </div>
 
--- frontend/frontend/src/components/Register.tsx
+++ frontend/frontend/src/components/Register.tsx
@@ -82,7 +82,7 @@ export const Register = () => {
                 onClick={() => setShowPassword((prev) => !prev)}
                 className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
               >
-                {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+                {showPassword ? <Eye size={18} /> : <EyeOff size={18} />}
               </button>
             </div>
 
@@ -99,7 +99,7 @@ export const Register = () => {
                 onClick={() => setShowConfirmPassword((prev) => !prev)}
                 className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
               >
-                {showConfirmPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+                {showConfirmPassword ? <Eye size={18} /> : <EyeOff size={18} />}
               </button>
             </div>
             {errorMessage && (
